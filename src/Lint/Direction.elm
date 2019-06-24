854e0412dbc4a8e1224bea2071c98d0e09b907e3
Jeroen Engels <jfm.engels@gmail.com>
2019-06-24 23:40:26 +0200

    Rework the documentation for Direction

--- src/Lint/Direction.elm
+++ src/Lint/Direction.elm
@@ -10,25 +10,29 @@ module Lint.Direction exposing (Direction(..))
 -}
 
 
-{-| When visiting the AST, nodes are visited twice:
+{-| When visiting the AST, nodes are visited twice: once on `Enter`, before the
+children of the node will be visited, and once on `Exit`, after the children of
+the node have been visited.
 
-  - on Enter, before the children of the node will be visited
-
-  - on Exit, after the children of the node have been visited
+In most cases, you'll only want to handle the `Enter` case, but in some cases,
+you'll want to visit a `Node` after having seen it's children. For instance, if
+you're trying to detect the unused variables defined inside of a `let in` expression,
+you'll want to collect the declaration of variables, note which ones are used,
+and at the end of the block, report the ones that weren't used.
 
     expressionVisitor : Context -> Direction -> Node Expression -> ( List Error, Context )
     expressionVisitor context direction node =
-    case (direction, node) of
-    ( Enter, Expression.FunctionOrValue moduleName name) ->
-    ( [], markVariableAsUsed context name )
+        case ( direction, node ) of
+            ( Direction., Expression.FunctionOrValue moduleName name ) ->
+            ( [], markVariableAsUsed context name )
 
-              -- Find variables declared in `let .. in ..` expression
-              ( Enter, LetExpression letBlock ) ->
-                  ( [], registerVariables context letBlock )
+            -- Find variables declared in `let in` expression
+            ( Direction.Enter, LetExpression letBlock ) ->
+                ( [], registerVariables context letBlock )
 
-              -- When exiting the `let .. in ..` expression, report the variables that were not used.
-              ( Enter, LetExpression _ ) ->
-                  ( unusedVariables context |> List.map createError, context )
+            -- When exiting the `let in expression, report the variables that were not used.
+            ( Direction.Exit, LetExpression _ ) ->
+                ( unusedVariables context |> List.map createError, context )
 
 -}
 type Direction
