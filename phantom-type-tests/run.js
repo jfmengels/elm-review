912ed3ded429595e66db532431fe9137a7284417
Jeroen Engels <jfm.engels@gmail.com>
2020-03-24 17:15:55 +0100

    Make phantom type tests work when run from any directory

--- phantom-type-tests/run.js
+++ phantom-type-tests/run.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
+const path = require('path');
 const { execSync } = require("child_process");
 
 const files = fs.readdirSync(__dirname);
 const elmFiles = files.filter(file => file.endsWith(".elm"));
 
 elmFiles.forEach(elmFile => {
-  const expectedOutput = fs.readFileSync(`${elmFile.slice(0, -4)}.txt`, 'utf8');
+  const expectedOutput = fs.readFileSync(path.join(__dirname, `${elmFile.slice(0, -4)}.txt`), 'utf8');
 
   try {
     const output = execSync(`elm make ./${elmFile}`, {encoding:"utf8", stdio: 'pipe', cwd: __dirname}).toString();
