988a543f87baee49bb47f78fefb3a92a02b0a37d
Jeroen Engels <jfm.engels@gmail.com>
2020-08-19 23:35:43 +0200

    Add ways to use the lookup table

--- src/Review/ModuleNameLookupTable.elm
+++ src/Review/ModuleNameLookupTable.elm
@@ -1,7 +1,16 @@
 module Review.ModuleNameLookupTable exposing (..)
 
+import Dict
+import Elm.Syntax.ModuleName exposing (ModuleName)
+import Elm.Syntax.Range exposing (Range)
 import Review.ModuleNameLookupTable.Internal as Internal
 
 
 type alias ModuleNameLookupTable =
     Internal.ModuleNameLookupTable
+
+
+moduleName : ModuleNameLookupTable -> Range -> ModuleName
+moduleName (Internal.ModuleNameLookupTable dict) range =
+    Dict.get (Internal.toRangeLike range) dict
+        |> Maybe.withDefault [ "UNKNOWN" ]
--- src/Review/ModuleNameLookupTable/Internal.elm
+++ src/Review/ModuleNameLookupTable/Internal.elm
@@ -1,10 +1,24 @@
-module Review.ModuleNameLookupTable.Internal exposing (ModuleNameLookupTable(..), empty)
+module Review.ModuleNameLookupTable.Internal exposing (ModuleNameLookupTable(..), empty, toRangeLike)
+
+import Dict exposing (Dict)
+import Elm.Syntax.Range exposing (Range)
 
 
 type ModuleNameLookupTable
-    = ModuleNameLookupTable
+    = ModuleNameLookupTable (Dict RangeLike (List String))
 
 
 empty : ModuleNameLookupTable
 empty =
-    ModuleNameLookupTable
+    ModuleNameLookupTable Dict.empty
+
+
+type alias RangeLike =
+    ( ( Int, Int ), ( Int, Int ) )
+
+
+toRangeLike : Range -> RangeLike
+toRangeLike { start, end } =
+    ( ( start.row, start.column )
+    , ( end.row, end.column )
+    )
