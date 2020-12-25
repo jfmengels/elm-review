a34cb9f22eb2c7b71efc8e9bc13b98b1e3a58e2a
Jeroen Engels <jfm.engels@gmail.com>
2020-12-25 21:26:56 +0100

    Add test to remove unused exposing (..)

--- tests/NoImportingEverythingTest.elm
+++ tests/NoImportingEverythingTest.elm
@@ -88,4 +88,27 @@ b = a
                             ]
                           )
                         ]
+        , test "should remove unused exposing (..) if nothing was exposed" <|
+            \_ ->
+                [ """module A exposing (thing)
+import OtherModule exposing (..)
+b = c
+""", """module OtherModule exposing (..)
+a = 1
+""" ]
+                    |> Review.Test.runOnModules (rule [])
+                    |> Review.Test.expectErrorsForModules
+                        [ ( "A"
+                          , [ Review.Test.error
+                                { message = "Prefer listing what you wish to import and/or using qualified imports"
+                                , details = [ "When you import everything from a module it becomes harder to know where a function or a type comes from." ]
+                                , under = "(..)"
+                                }
+                                |> Review.Test.whenFixed """module A exposing (thing)
+import OtherModule
+b = a
+"""
+                            ]
+                          )
+                        ]
         ]
