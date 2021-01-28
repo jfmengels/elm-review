1b836cb8d321642060022e286585df267aea2c60
Jeroen Engels <jfm.engels@gmail.com>
2021-01-28 17:14:10 +0100

    Duplicate Binop module to internal

---
+++ src/Review/Internal/Binop.elm
@@ -0,0 +1,102 @@
+module Review.Type.Binop exposing
+    ( Binop
+    , associatedFunction
+    , associativity
+    , create
+    , documentation
+    , fromElmDocs
+    , name
+    , precedence
+    , setDocumentationAndType
+    , tipe
+    , toElmDocs
+    )
+
+import Elm.Docs
+import Review.Type as Type
+
+
+type Binop
+    = Binop
+        { name : String
+        , associatedFunction : String
+        , documentation : Maybe String
+        , tipe : Maybe Type.Type
+        , associativity : Elm.Docs.Associativity
+        , precedence : Int
+        }
+
+
+create :
+    { name : String
+    , associatedFunction : String
+    , documentation : Maybe String
+    , tipe : Maybe Type.Type
+    , associativity : Elm.Docs.Associativity
+    , precedence : Int
+    }
+    -> Binop
+create =
+    Binop
+
+
+setDocumentationAndType : { documentation : Maybe String, tipe : Maybe Type.Type } -> Binop -> Binop
+setDocumentationAndType params (Binop binop) =
+    Binop { binop | documentation = params.documentation, tipe = params.tipe }
+
+
+fromElmDocs : Elm.Docs.Binop -> Binop
+fromElmDocs binop =
+    Binop
+        { name = binop.name
+        , associatedFunction = ""
+        , documentation = Just binop.comment
+        , tipe = Just (Type.fromElmDocs binop.tipe)
+        , associativity = binop.associativity
+        , precedence = binop.precedence
+        }
+
+
+toElmDocs : Binop -> Maybe Elm.Docs.Binop
+toElmDocs (Binop binop) =
+    Maybe.map2
+        (\documentation_ tipe_ ->
+            { name = binop.name
+            , comment = documentation_
+            , tipe = tipe_
+            , associativity = binop.associativity
+            , precedence = binop.precedence
+            }
+        )
+        binop.documentation
+        (Maybe.andThen Type.toElmDocs binop.tipe)
+
+
+name : Binop -> String
+name (Binop binop) =
+    binop.name
+
+
+associatedFunction : Binop -> String
+associatedFunction (Binop binop) =
+    binop.associatedFunction
+
+
+documentation : Binop -> Maybe String
+documentation (Binop binop) =
+    binop.documentation
+
+
+tipe : Binop -> Maybe Type.Type
+tipe (Binop binop) =
+    binop.tipe
+
+
+associativity : Binop -> Elm.Docs.Associativity
+associativity (Binop binop) =
+    binop.associativity
+
+
+precedence : Binop -> Int
+precedence (Binop binop) =
+    binop.precedence
