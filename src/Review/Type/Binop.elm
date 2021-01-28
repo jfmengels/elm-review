ad40ca7bc970bae2f8fe9d38062de7a040f8fb82
Jeroen Engels <jfm.engels@gmail.com>
2021-01-28 17:48:59 +0100

    Document documentation

--- src/Review/Type/Binop.elm
+++ src/Review/Type/Binop.elm
@@ -50,8 +50,10 @@ create =
 
 {-| Get the name of a binary operation.
 
-    infix left  6 (+)  = add
-    -> "+"
+
+    infix left  6 (+) = add
+
+    --> "+"
 
 -}
 name : Binop -> String
@@ -59,10 +61,12 @@ name =
     Review.Internal.Binop.name
 
 
-{-| Get the name of a binary operation. For the `+` operator, that would be `+`.
+{-| Get the associatedFunction of a binary operation.
 
-    infix left  6 (+)  = add
-    -> "add"
+
+    infix left  6 (+) = add
+
+    --> "add"
 
 -}
 associatedFunction : Binop -> Maybe String
@@ -70,6 +74,24 @@ associatedFunction =
     Review.Internal.Binop.associatedFunction
 
 
+{-| Get the documentation of a binary operation.
+
+    {-| Add things.
+    -}
+
+    add : number -> number -> number
+    add =
+        Elm.Kernel.Basics.add
+
+    infix left  6 (+) = add
+
+    --> " Add things.\n"
+
+The leading `{-|` and trailing `-}` are stripped off, but the rest of the string remains as is. In the example above, the documentation would be `documentation\n`.
+
+The documentation will be `Nothing` if it is was missing.
+
+-}
 documentation : Binop -> Maybe String
 documentation =
     Review.Internal.Binop.documentation
