bbc4697d9f8a31e4200cb302dcbf146c8d58f222
Jeroen Engels <jfm.engels@gmail.com>
2021-03-07 16:33:19 +0100

    Duplicate

--- tests/NoUnsafeRegexFromLiteralTest.elm
+++ tests/NoUnsafeRegexFromLiteralTest.elm
@@ -166,10 +166,16 @@ a = SomeModule.Regex.fromLiteralFunc "^abc$"
 """
                     |> Review.Test.runWithProjectData project (rule configuration)
                     |> Review.Test.expectGlobalErrors
-                        [ { message = "DIFF Could not find SomeModule.Regex.fromLiteralFunc"
+                        [ { message = "Could not find SomeModule.Regex.fromLiteralFunc"
                           , details =
                                 [ "I want to provide guarantees on the use of this function, but I can't find it. It is likely that it was renamed, which prevents me from giving you these guarantees."
-                                , "DIFF You should rename it back or update this rule to the new name. If you do not use the function anymore, remove the rule."
+                                , "You should rename it back or update this rule to the new name. If you do not use the function anymore, remove the rule."
+                                ]
+                          }
+                        , { message = "Could not find SomeModule.Regex.fromLiteralFunc"
+                          , details =
+                                [ "I want to provide guarantees on the use of this function, but I can't find it. It is likely that it was renamed, which prevents me from giving you these guarantees."
+                                , "You should rename it back or update this rule to the new name. If you do not use the function anymore, remove the rule."
                                 ]
                           }
                         ]
