3792b9bb561867863e0ccb876462f743ea03f997
Jeroen Engels <jfm.engels@gmail.com>
2021-03-11 14:14:32 +0100

    Add elm/html

--- src/Review/Test/Dependencies.elm
+++ src/Review/Test/Dependencies.elm
@@ -1,9 +1,15 @@
-module Review.Test.Dependencies exposing (elmCore, elmParser, elmUrl)
+module Review.Test.Dependencies exposing (elmCore, elmHtml)
 
 import Review.Project.Dependency exposing (Dependency)
 import Review.Test.Dependencies.ElmCore
+import Review.Test.Dependencies.ElmHtml
 
 
 elmCore : Dependency
 elmCore =
     Review.Test.Dependencies.ElmCore
+
+
+elmHtml : Dependency
+elmHtml =
+    Review.Test.Dependencies.ElmHtml
