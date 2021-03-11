b2e514175e1e2308a57b678a8077977bef59c438
Jeroen Engels <jfm.engels@gmail.com>
2021-03-11 13:33:26 +0100

    Generate more

--- create-dependency.js
+++ create-dependency.js
@@ -39,6 +39,7 @@ function createFile([elmJson, docsJson]) {
 
 import Elm.Docs
 import Elm.Project
+import Elm.Type
 import Json.Decode as Decode
 import Json.Encode as Encode
 import Review.Project.Dependency as Dependency exposing (Dependency)
@@ -81,11 +82,11 @@ function formatModuleName(packageName) {
 
 function formatModule(moduleDoc) {
     return `{ name = "${moduleDoc.name}"
-      , comment = ""
-      , unions = []
-      , aliases = []
-      , values = []
-      , binops = []
+      , comment = ${formatComment(moduleDoc.comment)}
+      , unions = [ ${moduleDoc.unions.map(formatUnion).join("\n    , ")} ]
+      , aliases = [ ${moduleDoc.aliases.map(formatAlias).filter(Boolean).join("\n    , ")} ]
+      , values = [ ${moduleDoc.values.map(formatValue).filter(Boolean).join("\n    , ")} ]
+      , binops = [ ${moduleDoc.binops.map(formatBinop).filter(Boolean).join("\n    , ")} ]
       }`
     // if (JSON.stringify(moduleDoc).includes("initialize 4")) {
     //     console.log(JSON.stringify(moduleDoc).slice(2470, 2530))
@@ -96,6 +97,59 @@ function formatModule(moduleDoc) {
     // .join("\\\\n");
 }
 
+function formatUnion(union) {
+    return `{ name = "${union.name}"
+      , comment = ${formatComment(union.comment)}
+      , args = ${JSON.stringify(union.args)}
+      , tags = [ ${union.cases.map(
+        (([name, type]) =>
+            `( "${name}", ${JSON.stringify(type)} |> Decode.decodeString Elm.Type.decoder |> Result.toMaybe |> Maybe.withDefault [] )`)
+    ).join("\n    , ")} ]
+      }`
+}
+
+function formatComment(comment) {
+    const withEscapedTripleQuotes = comment.split(`"""`).join(`\\"\\"\\"`);
+    return `"""${withEscapedTripleQuotes}"""`
+}
+
+function formatAlias(alias) {
+    return ""
+    return `{
+    name = "${alias.name}"
+        , comment = ${formatComment(alias.comment)}
+            , args = ${JSON.stringify(alias.unions)}
+      , cases = [${alias.aliases.map(formatAlias).join("\n    , ")}]
+        , values = [${alias.values.map(formatValue).join("\n    , ")}]
+        , binops = [${alias.binops.map(formatBinop).join("\n    , ")}]
+} `
+}
+
+function formatValue(value) {
+    return ""
+
+    return `{
+    name = "${value.name}"
+        , comment = ${formatComment(value.comment)}
+            , unions = [${value.unions.map(formatUnion).join("\n    , ")}]
+            , aliases = [${value.aliases.map(formatAlias).join("\n    , ")}]
+            , values = [${value.values.map(formatValue).join("\n    , ")}]
+            , binops = [${value.binops.map(formatBinop).join("\n    , ")}]
+} `
+}
+
+
+function formatBinop(binop) {
+    return ""
+    return `{
+    name = "${binop.name}"
+        , comment = ${formatComment(binop.comment)}
+            , unions = [${binop.unions.map(formatUnion).join("\n    , ")}]
+            , aliases = [${binop.aliases.map(formatAlias).join("\n    , ")}]
+            , values = [${binop.values.map(formatValue).join("\n    , ")}]
+            , binops = [${binop.binops.map(formatBinop).join("\n    , ")}]
+} `
+}
 
 downloadFiles()
     .then(createFile)
