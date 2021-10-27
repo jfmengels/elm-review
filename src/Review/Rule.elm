46693801d5d5829a448ebc21d2e317f43d538b39
Jeroen Engels <jfm.engels@gmail.com>
2021-10-27 17:37:43 +0200

    Inline functionToExpression

--- src/Review/Rule.elm
+++ src/Review/Rule.elm
@@ -4434,7 +4434,7 @@ visitDeclaration declarationVisitorsOnEnter declarationVisitorsOnExit expression
         Declaration.FunctionDeclaration function ->
             ( [], moduleContext )
                 |> visitWithListOfVisitors declarationVisitorsOnEnter node
-                |> accumulate (visitExpression expressionRelatedVisitors (functionToExpression function))
+                |> accumulate (visitExpression expressionRelatedVisitors (Node.value function.declaration).expression)
                 |> visitWithListOfVisitors declarationVisitorsOnExit node
 
         _ ->
