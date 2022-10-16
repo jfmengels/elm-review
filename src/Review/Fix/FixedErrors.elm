e42f33f6dccf6c84349048e3b9f105638cff6d04
Jeroen Engels <jfm.engels@gmail.com>
2022-10-16 11:05:04 +0200

    Make FixedErrors opaque

--- src/Review/Fix/FixedErrors.elm
+++ src/Review/Fix/FixedErrors.elm
@@ -4,23 +4,25 @@ import Dict exposing (Dict)
 import Review.Error exposing (ReviewError)
 
 
-type alias FixedErrors =
-    Dict String (List ReviewError)
+type FixedErrors
+    = FixedErrors (Dict String (List ReviewError))
 
 
 empty : FixedErrors
 empty =
-    Dict.empty
+    FixedErrors Dict.empty
 
 
 insert : ReviewError -> FixedErrors -> FixedErrors
-insert ((Review.Error.ReviewError { filePath }) as error) fixedErrors =
-    Dict.update
-        filePath
-        (\errors -> Just (error :: Maybe.withDefault [] errors))
-        fixedErrors
+insert ((Review.Error.ReviewError { filePath }) as error) (FixedErrors fixedErrors) =
+    FixedErrors
+        (Dict.update
+            filePath
+            (\errors -> Just (error :: Maybe.withDefault [] errors))
+            fixedErrors
+        )
 
 
 hasChanged : FixedErrors -> FixedErrors -> Bool
-hasChanged before after =
+hasChanged (FixedErrors before) (FixedErrors after) =
     before /= after
