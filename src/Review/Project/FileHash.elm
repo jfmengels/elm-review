53d60d1df9ee4bf6d4deb08119c0e11b3c6f773d
Jeroen Engels <jfm.engels@gmail.com>
2022-11-09 09:50:16 +0100

    Add FileHash.hash

--- src/Review/Project/FileHash.elm
+++ src/Review/Project/FileHash.elm
@@ -1,5 +1,12 @@
-module Review.Project.FileHash exposing (FileHash)
+module Review.Project.FileHash exposing (FileHash, hash)
+
+import Murmur3
 
 
 type FileHash
     = FileHash Int
+
+
+hash : String -> FileHash
+hash source =
+    FileHash (Murmur3.hashString 0 source)
