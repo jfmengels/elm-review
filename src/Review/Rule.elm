254d5f63e198287f326372d7a3cb2f84c8e753d9
Jeroen Engels <jfm.engels@gmail.com>
2023-04-01 09:03:53 +0200

    Add TODO

--- src/Review/Rule.elm
+++ src/Review/Rule.elm
@@ -4660,6 +4660,7 @@ computeModule ({ ruleProjectVisitors, module_, project, incoming } as params) =
 
         ( moduleNameLookupTable, newProject ) =
             -- TODO Try to compute this later, once we know for sure that the rules we will run want the lookup table
+            -- TODO If the file has changed, then compute the module docs anyway.
             if requestedData.moduleNameLookupTable then
                 Review.ModuleNameLookupTable.Compute.compute moduleName module_ project
 
