06760b9076abefb04c17204ba36417ef3a8f6edd
Jeroen Engels <jfm.engels@gmail.com>
2023-06-03 00:41:49 +0200

    Inline constant argument

--- src/Review/Rule.elm
+++ src/Review/Rule.elm
@@ -5620,7 +5620,7 @@ createRuleProjectVisitor schema initialProject ruleData initialCache =
             RuleProjectVisitor
                 { elmJsonVisitor = createProjectVisitor schema hidden schema.elmJsonVisitor ElmJsonStep ValidProject.elmJsonHash .elmJson (\entry -> raiseCache { cache | elmJson = Just entry }) (\() -> raise hidden)
                 , readmeVisitor = createProjectVisitor schema hidden schema.readmeVisitor ReadmeStep ValidProject.readmeHash .readme (\entry -> raiseCache { cache | readme = Just entry }) (\() -> raise hidden)
-                , extraFilesVisitor = createExtraFilesVisitor schema hidden schema.extraFilesVisitor (\entry -> raiseCache { cache | extraFiles = Just entry }) (\() -> raise hidden)
+                , extraFilesVisitor = createExtraFilesVisitor schema hidden (\entry -> raiseCache { cache | extraFiles = Just entry }) (\() -> raise hidden)
                 , dependenciesVisitor = createDependenciesVisitor schema hidden.ruleData raiseCache cache { allVisitor = schema.dependenciesVisitor, directVisitor = schema.directDependenciesVisitor }
                 , createModuleVisitorFromProjectVisitor = createModuleVisitorFromProjectVisitor schema raiseCache hidden
                 , finalProjectEvaluation = createFinalProjectEvaluationVisitor schema hidden.ruleData raiseCache cache
@@ -5719,17 +5719,16 @@ createProjectVisitor schema hidden maybeVisitor step computeContentHash cacheGet
 createExtraFilesVisitor :
     ProjectRuleSchemaData projectContext moduleContext
     -> RuleProjectVisitorHidden projectContext
-    -> Maybe (data -> projectContext -> ( List (Error {}), projectContext ))
     -> (ExtraFilesCache projectContext -> RuleProjectVisitor)
     -> (() -> RuleProjectVisitor)
     ->
         Maybe
             (ValidProject
-             -> data
+             -> List { content : String, path : String }
              -> ( List (Error {}), RuleProjectVisitor )
             )
-createExtraFilesVisitor schema hidden maybeVisitor toRuleProjectVisitor toRuleProjectVisitorWithoutChangingCache =
-    case maybeVisitor of
+createExtraFilesVisitor schema hidden toRuleProjectVisitor toRuleProjectVisitorWithoutChangingCache =
+    case schema.extraFilesVisitor of
         Nothing ->
             Nothing
 
