174e586fa5b271552ca0e7624e327ec44a302fab
Jeroen Engels <jfm.engels@gmail.com>
2023-06-03 00:40:36 +0200

    Inline constant argument

--- src/Review/Rule.elm
+++ src/Review/Rule.elm
@@ -5620,7 +5620,7 @@ createRuleProjectVisitor schema initialProject ruleData initialCache =
             RuleProjectVisitor
                 { elmJsonVisitor = createProjectVisitor schema hidden schema.elmJsonVisitor ElmJsonStep ValidProject.elmJsonHash .elmJson (\entry -> raiseCache { cache | elmJson = Just entry }) (\() -> raise hidden)
                 , readmeVisitor = createProjectVisitor schema hidden schema.readmeVisitor ReadmeStep ValidProject.readmeHash .readme (\entry -> raiseCache { cache | readme = Just entry }) (\() -> raise hidden)
-                , extraFilesVisitor = createExtraFilesVisitor schema hidden schema.extraFilesVisitor ExtraFilesStep (\entry -> raiseCache { cache | extraFiles = Just entry }) (\() -> raise hidden)
+                , extraFilesVisitor = createExtraFilesVisitor schema hidden schema.extraFilesVisitor (\entry -> raiseCache { cache | extraFiles = Just entry }) (\() -> raise hidden)
                 , dependenciesVisitor = createDependenciesVisitor schema hidden.ruleData raiseCache cache { allVisitor = schema.dependenciesVisitor, directVisitor = schema.directDependenciesVisitor }
                 , createModuleVisitorFromProjectVisitor = createModuleVisitorFromProjectVisitor schema raiseCache hidden
                 , finalProjectEvaluation = createFinalProjectEvaluationVisitor schema hidden.ruleData raiseCache cache
@@ -5720,7 +5720,6 @@ createExtraFilesVisitor :
     ProjectRuleSchemaData projectContext moduleContext
     -> RuleProjectVisitorHidden projectContext
     -> Maybe (data -> projectContext -> ( List (Error {}), projectContext ))
-    -> StepToComputeContext
     -> (ExtraFilesCache projectContext -> RuleProjectVisitor)
     -> (() -> RuleProjectVisitor)
     ->
@@ -5729,7 +5728,7 @@ createExtraFilesVisitor :
              -> data
              -> ( List (Error {}), RuleProjectVisitor )
             )
-createExtraFilesVisitor schema hidden maybeVisitor step toRuleProjectVisitor toRuleProjectVisitorWithoutChangingCache =
+createExtraFilesVisitor schema hidden maybeVisitor toRuleProjectVisitor toRuleProjectVisitorWithoutChangingCache =
     case maybeVisitor of
         Nothing ->
             Nothing
@@ -5739,7 +5738,7 @@ createExtraFilesVisitor schema hidden maybeVisitor step toRuleProjectVisitor toR
                 (\project data ->
                     let
                         ( baseInputContextHash, inputContext ) =
-                            findInitialInputContext hidden.cache step schema.initialProjectContext
+                            findInitialInputContext hidden.cache ExtraFilesStep schema.initialProjectContext
 
                         inputContextHash : ComparableContextHash projectContext
                         inputContextHash =
