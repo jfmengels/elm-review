89728bc745e1be1d23483245730620708d8c9b14
Jeroen Engels <jfm.engels@gmail.com>
2024-03-19 22:28:23 +0100

    Report CSS classes

--- tests/NoUnknownCssClassesTest.elm
+++ tests/NoUnknownCssClassesTest.elm
@@ -8,24 +8,32 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "NoUnknownCssClasses"
-        [ test "should not report an error when REPLACEME" <|
+        [ test "should not report an error when strings don't seem to be CSS classes" <|
             \() ->
                 """module A exposing (..)
-a = 1
+import Html
+import Html.Attributes as Attr
+
+view model =
+    Html.span [] [ Html.text "ok" ]
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectNoErrors
-        , test "should report an error when REPLACEME" <|
+        , test "should report an error when encountering an unknown CSS class through Html.Attributes.class" <|
             \() ->
                 """module A exposing (..)
-a = 1
+import Html
+import Html.Attributes as Attr
+
+view model =
+    Html.span [ Attr.class "unknown" ] []
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
                             { message = "REPLACEME"
                             , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
+                            , under = "\"unknown\""
                             }
                         ]
         ]
