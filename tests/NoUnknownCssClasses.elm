82bd2451285f1e7abd8c321e805cc27ca59afaa5
Jeroen Engels <jfm.engels@gmail.com>
2024-03-20 00:37:22 +0100

    Define css functions

--- tests/NoUnknownCssClasses.elm
+++ tests/NoUnknownCssClasses.elm
@@ -9,7 +9,9 @@ module NoUnknownCssClasses exposing
 
 -}
 
+import Dict exposing (Dict)
 import Elm.Syntax.Expression as Expression exposing (Expression)
+import Elm.Syntax.ModuleName exposing (ModuleName)
 import Elm.Syntax.Node as Node exposing (Node(..))
 import Elm.Syntax.Range as Range exposing (Range)
 import NoInconsistentAliases exposing (Config)
@@ -166,6 +168,34 @@ expressionVisitor node context =
 
         _ ->
             ( [], context )
+
+
+type CssArgument
+    = Literal String
+    | Variable Range
+
+
+type alias CssFunctions =
+    Dict
+        ( ModuleName, String )
+        ({ firstArgument : Node Expression, restOfArguments : List (Node Expression) } -> List CssArgument)
+
+
+cssFunctions : CssFunctions
+cssFunctions =
+    Dict.fromList
+        [ ( ( [ "Html", "Attributes" ], "class" ), \{ firstArgument } -> [ fromLiteral firstArgument ] )
+        ]
+
+
+fromLiteral : Node Expression -> CssArgument
+fromLiteral node =
+    case Node.value node of
+        Expression.Literal str ->
+            Literal str
+
+        _ ->
+            Variable (Node.range node)
 
 
 reportClasses : ModuleContext -> Range -> String -> Node Expression -> f -> List (Rule.Error {})
