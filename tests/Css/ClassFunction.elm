7af1d00dda4017ea21aceeb9d9dec155f78cb13c
Jeroen Engels <jfm.engels@gmail.com>
2024-03-25 22:54:44 +0100

    Move CSS class functions over to ClassFunctions

--- tests/Css/ClassFunction.elm
+++ tests/Css/ClassFunction.elm
@@ -1,4 +1,18 @@
-module Css.ClassFunction exposing (CssArgument(..), fromLiteral)
+module Css.ClassFunction exposing
+    ( CssArgument(..)
+    , fromLiteral
+    , baseCssFunctions
+    , firstArgumentIsClass, htmlAttributesAttribute, htmlAttributesClassList
+    )
+
+{-|
+
+@docs CssArgument
+@docs fromLiteral
+@docs baseCssFunctions
+@docs firstArgumentIsClass, htmlAttributesAttribute, htmlAttributesClassList
+
+-}
 
 import Dict exposing (Dict)
 import Elm.Syntax.Expression as Expression exposing (Expression)
@@ -21,3 +35,51 @@ fromLiteral node =
 
         _ ->
             Variable (Node.range node)
+
+
+baseCssFunctions : List ( String, { firstArgument : Node Expression, restOfArguments : List (Node Expression) } -> List CssArgument )
+baseCssFunctions =
+    [ ( "Html.Attributes.class", \{ firstArgument } -> [ fromLiteral firstArgument ] )
+    , ( "Svg.Attributes.class", \{ firstArgument } -> [ fromLiteral firstArgument ] )
+    , ( "Html.Attributes.classList", htmlAttributesClassList )
+    , ( "Html.Attributes.attribute", htmlAttributesAttribute )
+    ]
+
+
+firstArgumentIsClass : { firstArgument : Node Expression, restOfArguments : List (Node Expression) } -> List CssArgument
+firstArgumentIsClass { firstArgument } =
+    [ fromLiteral firstArgument ]
+
+
+htmlAttributesAttribute : { firstArgument : Node Expression, restOfArguments : List (Node Expression) } -> List CssArgument
+htmlAttributesAttribute { firstArgument, restOfArguments } =
+    case Node.value firstArgument of
+        Expression.Literal "class" ->
+            case restOfArguments of
+                [] ->
+                    [ MissingArgument 2 ]
+
+                classArgument :: _ ->
+                    [ fromLiteral classArgument ]
+
+        _ ->
+            []
+
+
+htmlAttributesClassList : { firstArgument : Node Expression, restOfArguments : List (Node Expression) } -> List CssArgument
+htmlAttributesClassList { firstArgument } =
+    case Node.value firstArgument of
+        Expression.ListExpr list ->
+            List.map
+                (\element ->
+                    case Node.value element of
+                        Expression.TupledExpression [ first, _ ] ->
+                            fromLiteral first
+
+                        _ ->
+                            Variable (Node.range element)
+                )
+                list
+
+        _ ->
+            [ fromLiteral firstArgument ]
