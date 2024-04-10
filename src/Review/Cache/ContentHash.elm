1d139cd385eb9d0bbae7ad0ab9facbea7fe4c925
Jeroen Engels <jfm.engels@gmail.com>
2024-04-10 20:45:39 +0200

    Add ContentHash.nil

--- src/Review/Cache/ContentHash.elm
+++ src/Review/Cache/ContentHash.elm
@@ -1,10 +1,15 @@
-module Review.Cache.ContentHash exposing (ContentHash, hash)
+module Review.Cache.ContentHash exposing (ContentHash, hash, nil)
 
 import Vendor.Murmur3 as Murmur3
 
 
 type ContentHash
     = ContentHash Int
+
+
+nil : ContentHash
+nil =
+    ContentHash 0
 
 
 hash : String -> ContentHash
