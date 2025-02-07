d8d353e5ffda191de3d1796c7056ae4b86a2e8ea
Jeroen Engels <jfm.engels@gmail.com>
2025-02-07 16:29:44 +0100

    Remove processing when parsing files

--- src/Review/FileParser.elm
+++ src/Review/FileParser.elm
@@ -1,26 +1,16 @@
 module Review.FileParser exposing (parse)
 
 import Elm.Parser as Parser
-import Elm.Processing
 import Elm.Syntax.File exposing (File)
-import Review.Dependencies
 
 
 {-| Parse source code into a AST.
 -}
 parse : String -> Result () File
 parse source =
-    case Parser.parse source of
+    case Parser.parseToFile source of
         Ok file ->
-            Ok (Elm.Processing.process elmProcessContext file)
+            Ok file
 
         Err _ ->
             Err ()
-
-
-elmProcessContext : Elm.Processing.ProcessContext
-elmProcessContext =
-    Elm.Processing.init
-        |> Elm.Processing.addDependency Review.Dependencies.elmCore
-        |> Elm.Processing.addDependency Review.Dependencies.elmUrl
-        |> Elm.Processing.addDependency Review.Dependencies.elmParser
