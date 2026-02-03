480cddc310138a2705ba3f489f4addb4c145c179
Jeroen Engels <jfm.engels@gmail.com>
2026-02-03 16:51:00 +0100

    Update test to properly recreate failing conditions
    for commit "Compute module docs when necessary"
    commit sha: c0fd291d87e17dd691985da7a1cae276daadae2a

--- tests/Review/Rule/CacheTest.elm
+++ tests/Review/Rule/CacheTest.elm
@@ -1,9 +1,12 @@
 module Review.Rule.CacheTest exposing (all)
 
 import Dict exposing (Dict)
+import Elm.Syntax.Expression as Expression exposing (Expression)
+import Elm.Syntax.Node as Node exposing (Node)
 import Expect
 import Json.Encode as Encode
 import NoUnused.Variables
+import Review.ModuleNameLookupTable as ModuleNameLookupTable exposing (ModuleNameLookupTable)
 import Review.Options
 import Review.Project as Project exposing (Project)
 import Review.Project.Valid as Valid
@@ -24,8 +27,8 @@ all =
                             |> Project.addModule
                                 { path = "A.elm"
                                 , source = """
-module A exposing (string)
-string = "string"
+module A exposing (value)
+value = "string"
 b = 1
 """
                                 }
@@ -34,7 +37,7 @@ b = 1
                                 , source = """
 module B exposing (a)
 import A exposing (..)
-a = string
+a = value
 """
                                 }
 
@@ -48,14 +51,53 @@ a = string
         , test "Changing a file after a run in an unrelated manner should yield the same results" <|
             \() ->
                 let
+                    clearModuleDocsCache : Project -> Project
+                    clearModuleDocsCache project_ =
+                        case Valid.parse project_ of
+                            Err error ->
+                                Debug.todo ("Project could not be parsed: " ++ Debug.toString error)
+
+                            Ok valid ->
+                                valid
+                                    |> Valid.clearElmDocsModuleFromProjectCacheTEST
+                                    |> Valid.toRegularProject
+
+                    rule : Rule
+                    rule =
+                        Rule.newModuleRuleSchemaUsingContextCreator "TestRule"
+                            (Rule.initContextCreator (\lookup () -> lookup)
+                                |> Rule.withModuleNameLookupTable
+                            )
+                            |> Rule.withExpressionEnterVisitor (\node lookup -> ( expressionVisitor node lookup, lookup ))
+                            |> Rule.fromModuleRuleSchema
+
+                    expressionVisitor : Node Expression -> ModuleNameLookupTable -> List (Rule.Error {})
+                    expressionVisitor node lookup =
+                        case Node.value node of
+                            Expression.FunctionOrValue [] "value" ->
+                                case ModuleNameLookupTable.moduleNameFor lookup node of
+                                    Just [ "A" ] ->
+                                        [ Rule.error
+                                            { message = "Message"
+                                            , details = [ "Details" ]
+                                            }
+                                            (Node.range node)
+                                        ]
+
+                                    _ ->
+                                        []
+
+                            _ ->
+                                []
+
                     project : Project
                     project =
                         Review.Test.Dependencies.projectWithElmCore
                             |> Project.addModule
                                 { path = "A.elm"
                                 , source = """
-module A exposing (string)
-string = "string"
+module A exposing (value)
+value = "string"
 """
                                 }
                             |> Project.addModule
@@ -63,13 +105,13 @@ string = "string"
                                 , source = """
 module B exposing (a)
 import A exposing (..)
-a = string
+a = value
 """
                                 }
 
                     reviewResult : { errors : List Rule.ReviewError, rules : List Rule, project : Project, extracts : Dict String Encode.Value, fixedErrors : Dict String (List Rule.ReviewError) }
                     reviewResult =
-                        Rule.reviewV3 Review.Options.defaults [ NoUnused.Variables.rule ] project
+                        Rule.reviewV3 Review.Options.defaults [ rule ] project
 
                     updatedProject : Project
                     updatedProject =
@@ -79,7 +121,7 @@ a = string
                                 , source = """
 module B exposing (a)
 import A exposing (..)
-a = string ++ "ok"
+a = value ++ "ok"
 """
                                 }
                             |> clearModuleDocsCache
@@ -88,15 +130,3 @@ a = string ++ "ok"
                     |> .errors
                     |> Expect.equal reviewResult.errors
         ]
-
-
-clearModuleDocsCache : Project -> Project
-clearModuleDocsCache project =
-    case Valid.parse project of
-        Err error ->
-            Debug.todo ("Project could not be parsed: " ++ Debug.toString error)
-
-        Ok valid ->
-            valid
-                |> Valid.clearElmDocsModuleFromProjectCacheTEST
-                |> Valid.toRegularProject
