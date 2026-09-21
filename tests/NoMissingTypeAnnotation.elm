13e83e2c78c571431be8776c4ee8ceb072416566
Jeroen Engels <jfm.engels@gmail.com>
2026-09-21 22:09:26 +0200

    NoMissingTypeAnnotation: Add an autofix

--- tests/NoMissingTypeAnnotation.elm
+++ tests/NoMissingTypeAnnotation.elm
@@ -7,8 +7,9 @@ module NoMissingTypeAnnotation exposing (rule)
 -}
 
 import Elm.Syntax.Declaration as Declaration exposing (Declaration)
-import Elm.Syntax.Node as Node exposing (Node)
+import Elm.Syntax.Node as Node exposing (Node(..))
 import Elm.TypeInference.Type
+import Review.Fix as Fix
 import Review.Rule as Rule exposing (Error, Rule)
 import TypeLookupTable exposing (TypeLookupTable)
 
@@ -58,7 +59,7 @@ elm-review --template jfmengels/elm-review-common/example --rules NoMissingTypeA
 rule : Rule
 rule =
     Rule.newModuleRuleSchemaUsingContextCreator "NoMissingTypeAnnotation" initialContext
-        |> Rule.withDeclarationEnterVisitor (\node ctx -> ( declarationVisitor node ctx, ctx ))
+        |> Rule.withDeclarationEnterVisitor declarationVisitor
         |> Rule.fromModuleRuleSchema
 
 
@@ -72,44 +73,42 @@ initialContext =
         |> Rule.withTypeLookupTable
 
 
-declarationVisitor : Node Declaration -> TypeLookupTable -> List (Error {})
+declarationVisitor : Node Declaration -> TypeLookupTable -> ( List (Error {}), TypeLookupTable )
 declarationVisitor declaration typeLookupTable =
     case Node.value declaration of
         Declaration.FunctionDeclaration function ->
             case function.signature of
                 Nothing ->
                     let
-                        name : Node String
-                        name =
+                        (Node range name) =
                             function.declaration
                                 |> Node.value
                                 |> .name
 
-                        inferredType : String
-                        inferredType =
-                            case
-                                TypeLookupTable.get (Node.range declaration) typeLookupTable
-                                    |> Tuple.first
-                            of
+                        ( inferredType, newTypeLookupTable ) =
+                            TypeLookupTable.get (Node.range declaration) typeLookupTable
+
+                        fix : List Fix.Edit
+                        fix =
+                            case inferredType of
                                 Just type_ ->
-                                    Elm.TypeInference.Type.toString type_
+                                    [ Fix.insertAt (Node.range declaration).start (name ++ " : " ++ Elm.TypeInference.Type.toString type_ ++ "\n") ]
 
                                 Nothing ->
-                                    "???"
+                                    []
                     in
-                    [ Rule.error
-                        { message = "Missing type annotation for `" ++ Node.value name ++ "`"
-                        , details =
-                            [ "Type annotations help you understand what happens in the code, and it will help the compiler give better error messages."
-                            , "Add the following line:"
-                            , "    " ++ Node.value name ++ " : " ++ inferredType
-                            ]
-                        }
-                        (Node.range name)
-                    ]
+                    ( [ Rule.errorWithFix
+                            { message = "Missing type annotation for `" ++ name ++ "`"
+                            , details = [ "Type annotations help you understand what happens in the code, and it will help the compiler give better error messages." ]
+                            }
+                            range
+                            fix
+                      ]
+                    , newTypeLookupTable
+                    )
 
                 Just _ ->
-                    []
+                    ( [], typeLookupTable )
 
         _ ->
-            []
+            ( [], typeLookupTable )
